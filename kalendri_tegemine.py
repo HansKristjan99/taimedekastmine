0a9a7eae03d88930c2f658d4ee6d35024efaf4e6
MonikaKoll <73592490+MonikaKoll@users.noreply.github.com>
2020-12-06 14:44:47 +0200

    erivärvi tärnid

--- kalendri_tegemine.py
+++ kalendri_tegemine.py
@@ -14,10 +14,12 @@ kal1 = calendar.TextCalendar(calendar.MONDAY)
 kalender1 = kal1.formatmonth(time_now.year, time_now.month)
 kal2 = calendar.TextCalendar(calendar.MONDAY)
 kalender2 = kal2.formatmonth(time_after.year, time_after.month)
+värvid = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray']
 
 def kalender(intervall):
     global kalender1
     global kalender2
+    global värvid
     paev, kuu, aasta = time_now.day, time_now.now().month, time_now.now().year
     kuu2, aasta2 = time_after.month, time_after.year
     for match in re.finditer(f'{time_now.year}', kalender1):
@@ -33,9 +35,13 @@ def kalender(intervall):
         punased2.append(paev)
         paev += intervall
     for el in punased1:
-        kalender1 = re.sub(rf'\b{el}\b', str(el) + colored('*','red'), kalender1, 1)
+        kalender1 = re.sub(rf'\b{el}\b', str(el) + colored('*',värvid[0]), kalender1, 1)
     for el in punased2:
-        kalender2 = re.sub(rf'\b{el}\b', str(el) + colored('*','red'), kalender2, 1)
+        kalender2 = re.sub(rf'\b{el}\b', str(el) + colored('*',värvid[0]), kalender2, 1)
+    try:
+        värvid.pop(0)
+    except:
+        värvid = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray']
     return (kalender1+kalender2)
 
 database = f"{path}\\pythonsqlite.db"
